598729d9e54613018464764a3a94e6dbe0f917d2
ehatshamriaz <64935324+ehatshamriaz@users.noreply.github.com>
2020-07-02 03:26:42 +0500

    game Enivronment  design completed

---
+++ Roll A Ball/Assets/Scripts/strTextLabel.cs
@@ -0,0 +1,16 @@
+﻿ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class strTextLabel : MonoBehaviour {
+	public Text nameLable;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 namePose = Camera.main.WorldToScreenPoint(this.transform.position);
+		nameLable.transform.position = namePose;
+	}
+}
